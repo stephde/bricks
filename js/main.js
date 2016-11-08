6716ff9908a1ca724fd3536d95e3f9abb74d2b32
Stephan Detje <stephan.detje@student.hpi.de>
2016-11-08 21:21:33 +0100

    create basic dev setup

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// main module for bricks game
+
+function startGame() {
+    console.log("games has started")
+
+    return true
+}
---
+++ test/mainSpec.js
@@ -0,0 +1,10 @@
+
+describe("inital setup was successful", function() {
+    it("should executes the test", function() {
+        expect(true).toBeTruthy()
+    })
+
+    it("should start the game", function() {
+        
+    })
+})
